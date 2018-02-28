7c49f5a08b10b8cba215d91ab6c65e07a458da78
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2018-02-28 22:40:23 +0100

    Try to mount pv

--- k8s/launch-calib.sh
+++ k8s/launch-calib.sh
@@ -8,4 +8,5 @@ CONFIGMAP_DIR="${DIR}/configmap"
 kubectl delete configmap --ignore-not-found=true config-run
 kubectl create configmap --from-file="$CONFIGMAP_DIR/run.sh" config-run
 
+kubectl apply -f "${DIR}/yaml/cinder-pv.yaml"
 kubectl apply -f "${DIR}/yaml/job.yaml"
