3a9bb8f88881d3e5696f3ad0c2acbf062bc1fc7b
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2018-03-09 10:57:49 +0100

    Run other process

--- k8s/kubectl/admin/configmap/run.sh
+++ k8s/kubectl/admin/configmap/run.sh
@@ -1,5 +1,20 @@
+set -e
+set -x
+
 export SEDMREDUXPATH="/mnt/data/redux"
 
 DATE=20180306
 
+NPROC=$(nproc)
+
 ccd_to_cube.py "$DATE" --tracematch --hexagrid
+
+date
+echo "derive_wavesolution"
+derive_wavesolution.py "$DATE" --nsub "$NPROC"
+
+date
+echo "ccd_to_cube --flat"
+ccd_to_cube.py "$DATE" --flat
+
+date
